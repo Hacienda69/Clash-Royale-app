47e5abf37376b1eb821e3d984d3075d874934ad1
Marc Escandell <99950510+MrMonkey420@users.noreply.github.com>
2024-01-03 19:17:08 +0100

    Added AppBar to SearchScreen

--- WatchApp/lib/screens/SearchScreen.dart
+++ WatchApp/lib/screens/SearchScreen.dart
@@ -1,12 +1,59 @@
+import 'package:WatchApp/screens/mainmenu.dart';
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget{
+class SearchScreen extends StatelessWidget {
   const SearchScreen({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          shadowColor: Colors.black,
+          elevation: 10,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  //minimumSize: const Size(30, 30),
+                  //maximumSize: const Size(30, 30),
+                ),
+                child: const Icon(
+                  Icons.home,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+              const Text(
+                "WatchWeb",
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
