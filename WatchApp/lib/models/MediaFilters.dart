f5d8582246afad87519bf2b90f38152ba2180c57
Marc Escandell <99950510+MrMonkey420@users.noreply.github.com>
2024-01-14 15:02:53 +0100

    More filters
    Filters: name, rank, genre, rating and year

--- WatchApp/lib/models/MediaFilters.dart
+++ WatchApp/lib/models/MediaFilters.dart
@@ -2,7 +2,7 @@ import 'package:WatchApp/models/APImovies.dart';
 import 'package:WatchApp/models/APIseries.dart';
 
 class Filters {
-  // --------- FILTER BY GENRE ---------
+  // --------- FILTER BY GENRE --------- ---------------------------------------------------------------------------------------
   //Returns a list of all the movies where 1 of their 3 genres coincide with the target one
   static List<Movies> filterMoviesByGenre(
       String targetGenre, List<Movies> list) {
@@ -25,9 +25,10 @@ class Filters {
     return filteredList;
   }
 
-  // --------- FILTER BY TITLE ---------
+  // --------- FILTER BY TITLE --------- ---------------------------------------------------------------------------------------
   // Returns a list of all the movies their titles coincide with "targetTitle"
-  static List<Movies> filterMoviesByTitle(String targetTitle, List<Movies> list) {
+  static List<Movies> filterMoviesByTitle(
+      String targetTitle, List<Movies> list) {
     List<Movies> filteredList = [];
 
     for (var movie in list) {
@@ -39,7 +40,8 @@ class Filters {
   }
 
   // Returns a list of all the series their titles coincide with "targetTitle"
-  static List<Series> filterSeriesByTitle(String targetTitle, List<Series> list) {
+  static List<Series> filterSeriesByTitle(
+      String targetTitle, List<Series> list) {
     List<Series> filteredList = [];
 
     for (var series in list) {
@@ -49,8 +51,70 @@ class Filters {
     }
     return filteredList;
   }
-  
-  // --------- FILTER BY YEAR ---------
-  // --------- FILTER BY RANK ---------
-  // --------- FILTRE BY RATING ---------
+
+  // --------- FILTER BY YEAR --------- ---------------------------------------------------------------------------------------
+  // ONLY MOVIES HAVE YEARS
+  // Returns a list where all the movies years coincide with the target one
+  static List<Movies> filterMoviesByYear(int targetYear, List<Movies> list) {
+    List<Movies> filteredList = [];
+
+    for (var movies in list) {
+      if (movies.year == targetYear) {
+        filteredList.add(movies);
+      }
+    }
+    return filteredList;
+  }
+
+  // --------- FILTER BY RANK --------- ---------------------------------------------------------------------------------------
+  // Returns a list of movies where their ranks are the same as the target
+  static List<Movies> filterMoviesByRank(int targetRank, List<Movies> list) {
+    List<Movies> filteredList = [];
+
+    for (var movie in list) {
+      if (movie.rank == targetRank) {
+        filteredList.add(movie);
+      }
+    }
+    return filteredList;
+  }
+
+  // Returns a list of series where their ranks are the same as the target
+  static List<Series> filterSeriesByRank(int targetRank, List<Series> list) {
+    List<Series> filteredList = [];
+
+    for (var series in list) {
+      if (series.rank == targetRank) {
+        filteredList.add(series);
+      }
+    }
+    return filteredList;
+  }
+
+  // --------- FILTRE BY RATING --------- ---------------------------------------------------------------------------------------
+  // Returns a list of movies where their ratings are the same as the target
+  static List<Series> filterSeriesByRating(
+      double targetRating, List<Series> list) {
+    List<Series> filteredList = [];
+
+    for (var series in list) {
+      if (series.rating == targetRating) {
+        filteredList.add(series);
+      }
+    }
+    return filteredList;
+  }
+
+  // Returns a list of series where their ratings are the same as the target
+  static List<Movies> filterMoviesByRating(
+      double targetRating, List<Movies> list) {
+    List<Movies> filteredList = [];
+
+    for (var movie in list) {
+      if (movie.rating == targetRating) {
+        filteredList.add(movie);
+      }
+    }
+    return filteredList;
+  }
 }
